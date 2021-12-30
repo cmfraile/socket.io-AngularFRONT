f8d5fdeaa8186ad6f8e152b94007f771ca382c50
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-30 15:51:36 +0100

    204

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { io,Socket} from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'socketiofront';
+  socket = io('http://localhost:8000');
+  
+  constructor(){
+    this.socket.on('connect',() => {
+      console.log("conectado");
+    });
+  }
+
 }
