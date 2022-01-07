9011938019b9f95790238f316675f845aa713ce2
Carlos Fraile <carlosfrailegomez93@gmail.com>
2022-01-07 15:46:08 +0100

    empezamos a implantar sockets

--- src/app/componentes/appcola/servicioappcola.service.ts
+++ src/app/componentes/appcola/servicioappcola.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import arrayShuffle from 'array-shuffle';
+import { io } from 'socket.io-client';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -13,8 +14,14 @@ export class ServicioappcolaService {
     dos : {},
     tres : {},
   };
+  socket = io('http://localhost:8000');
+  conexionstatus:boolean = false;
   
-  constructor( private _hc:HttpClient ){}
+  
+  constructor( private _hc:HttpClient ){
+    this.socket.on('connect',() => {this.conexionstatus = true});
+    this.socket.on('disconnect',() => {this.conexionstatus = false});
+  }
 
   traerpersonajes(){
     return this._hc.get('https://digimon-api.herokuapp.com/api/digimon').pipe(
