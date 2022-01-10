c886a09958c1e229674f7ed2a9bca8c2085da8c7
Carlos Fraile <carlosfrailegomez93@gmail.com>
2022-01-10 11:18:22 +0100

    terminada la practica de fundamentos de callback

--- src/app/componentes/callbacks/callbacks.component.ts
+++ src/app/componentes/callbacks/callbacks.component.ts
@@ -11,10 +11,18 @@ export class CallbacksComponent implements OnInit {
 
   socket = io('http://localhost:8000');
   conexionstatus:boolean = false;
+  data:any[] = [];
 
   constructor( public _l:Location ){
     this.socket.on('connect',() => {this.conexionstatus = true});
     this.socket.on('disconnect',() => {this.conexionstatus = false});
+  }
+
+  emision(){
+    this.socket.emit('angular','mensaje de ida',(id:any) => {
+      console.log(id,this.data);
+      this.data.push(id);
+    });
   }
 
   ngOnInit(): void {}
